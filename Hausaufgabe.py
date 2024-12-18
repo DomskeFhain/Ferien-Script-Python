b5072587d8e9c289850a302e7c9786a595bc6d65
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2024-12-18 14:42:06 +0100

    Create Hausaufgabe.py

---
+++ Hausaufgabe.py
@@ -0,0 +1,14 @@
+import datetime
+
+def CurrentDayTime():
+    return datetime.datetime.now
+def whenTheYearEnds():
+    
+    userinput = input("Wähle eine Aktion (Aktuelles Datum - 1, Wann das Jahr endet - 2 )")
+
+    if userinput == "1"
+    result = CurrentDayTime()
+    elif == "2"
+    result whenTheYearEnds
+    else:
+    "Ungültige Eingabe"
